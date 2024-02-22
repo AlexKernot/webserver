f80c6568147cb8a8f3e84b0f283ea7c7362b8141
Alex Kernot <alex.kernot04@gmail.com>
2024-02-22 18:18:19 +1030

    Fix: change itos logic and handle edge cases

--- webserv/src/itos.cpp
+++ webserv/src/itos.cpp
@@ -6,33 +6,32 @@
 /*   By: akernot <akernot@student.42Adel.org.au>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 13:28:37 by akernot           #+#    #+#             */
-/*   Updated: 2024/02/13 15:14:55 by akernot          ###   ########.fr       */
+/*   Updated: 2024/02/22 18:11:30 by akernot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "itos.hpp"
+
 #include <string>
-
-static int reverse(int n) {
-        int retval = 0;
-
-        while (n != 0) {
-                retval *= 10;
-                retval += n % 10;
-                n /= 10;
-        }
-        return retval;
-}
 
 std::string itos(int n)
 {
-        std::string retval;
-        int reversed = reverse(n);
+        long num = n;
+        std::string tmp;
+        bool negative = false;
 
-        if (n == 0)
+        if (num == 0)
                 return "0";
-        while (reversed != 0) {
-                retval += (reversed % 10) + '0';
-                reversed /= 10;
+        if (num < 0) {
+                num = -num;
+                negative = true;
         }
+        while (num != 0) {
+                tmp += (num % 10) + '0';
+                num /= 10;
+        }
+        if (negative)
+                tmp += "-";
+        std::string retval(tmp.rbegin(), tmp.rend());
         return retval;
 }
